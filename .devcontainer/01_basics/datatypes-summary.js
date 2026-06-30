60b4f550b7f03139556248beb18b962ca48c72c7
Naina Rawat <rawatnaina253@gmail.com>
2026-06-30 13:38:49 +0530

    datatypes symmary

---
+++ .devcontainer/01_basics/datatypes-summary.js
@@ -0,0 +1,29 @@
+//notes in copy
+
+const score = 100
+const scoreValue = 100.3
+ constisLoggedIn  = false 
+ const outsideTemp = null
+ let userEmail ;
+
+ const id = Symbol('213')
+
+ const anotherId = Symbol('234')
+
+ console.log( id === anotherId);
+bigInt = 12345677891238830n
+ //referrence 
+ // Array , objects , Functions 
+
+ const heros = ["shaktiman", " naagraj", "doga"];
+ let myObj = {
+    name : "Naina",
+    age :22,
+
+ }
+ // function
+ const myFunction = function() {
+    console.log("Hello, World!");
+ }
+
+ console.log( typeof heros);
