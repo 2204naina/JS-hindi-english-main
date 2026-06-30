48bcb922f5fd2fa9095614945d4ea6db7be94542
Naina Rawat <rawatnaina253@gmail.com>
2026-06-30 12:38:57 +0530

    comparison or conver

--- .devcontainer/01_basics/03_conversionoperations.js
+++ .devcontainer/01_basics/03_conversionoperations.js
@@ -6,7 +6,25 @@
 //"33" =  33
 //"331bc"= NaN
 //true = 1;false =0
-let isLoggedIn = 0
+//let isLoggedIn = 0
 
-let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+//let booleanIsLoggedIn = Boolean(isLoggedIn);
+//console.log(booleanIsLoggedIn);
+
+
+//let str1 = "hello"
+//let str2 = "Naina"
+//let //str3 =str1 + str2
+//console.log(str3);
+//console.log(1+"3");
+//console.log("1"+3);
+//console.log(1+3);
+//console.log((3+4) *5 % 3);
+//console.log(+"")
+
+
+let num1,num2,num3
+num1=num2=num3 = 2+2+2
+let gamecounter =100 
+gamecounter++;
+console.log(gamecounter);
